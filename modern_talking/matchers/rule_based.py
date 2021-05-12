53a3d0e45578d1b0e4886ea67fa6594306213abd
Jan Heinrich Reimer <heinrich@reimer.family>
2021-05-12 19:16:20 +0200

    Move term overlap baseline

---
+++ modern_talking/matchers/rule_based.py
@@ -0,0 +1,30 @@
+from nltk.tokenize import word_tokenize
+
+from modern_talking.matchers import UntrainedMatcher
+from modern_talking.model import Dataset, Labels, Argument, KeyPoint, Label
+
+
+class TermOverlapMatcher(UntrainedMatcher):
+    """
+    Match argument key point pairs if their terms overlap.
+    """
+    name = "term-overlap"
+
+    @staticmethod
+    def term_overlap(arg: Argument, kp: KeyPoint) -> Label:
+        """
+        Calculate term overlap between an argument and key point
+        based on overlapping terms, i.e., terms that occur in the argument's
+        and the key point's text.
+        """
+        arg_terms = set(word_tokenize(arg.text))
+        kp_terms = set(word_tokenize(kp.text))
+        overlapping_terms = arg_terms.intersection(kp_terms)
+        return len(overlapping_terms) / min(len(arg_terms), len(kp_terms))
+
+    def predict(self, data: Dataset) -> Labels:
+        return {
+            (arg.id, kp.id): self.term_overlap(arg, kp)
+            for arg in data.arguments
+            for kp in data.key_points
+        }
