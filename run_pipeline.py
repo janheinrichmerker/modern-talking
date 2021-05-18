80fab659dae33ba4a6dae705b0af62adf2306f72
Jan Heinrich Reimer <heinrich@reimer.family>
2021-05-18 15:17:09 +0200

    Update pipeline runner

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,27 +1,31 @@
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
-from typing import List
 
 from modern_talking.evaluation import Metric
 from modern_talking.evaluation.f_measure import FMeasure
 from modern_talking.evaluation.precision import Precision
 from modern_talking.evaluation.recall import Recall
+from modern_talking.evaluation.track_1 import Track1Metric
 from modern_talking.matchers import Matcher
 from modern_talking.matchers.baselines import TopicStanceMatcher, RandomMatcher
-from modern_talking.matchers.rule_based import TermOverlapMatcher
+from modern_talking.matchers.rule_based import TermOverlapMatcher, \
+    StemmedTermOverlapMatcher
 from modern_talking.pipeline import Pipeline
 
-matchers: List[Matcher] = [
+matchers = (
     TopicStanceMatcher(),
     RandomMatcher(),
     TermOverlapMatcher(),
-]
+    StemmedTermOverlapMatcher(),
+)
 
-metrics: List[Metric] = [
+metrics = (
     Precision(),
     Recall(),
-    FMeasure(alpha=1)
-]
+    FMeasure(alpha=1),
+    Track1Metric(relaxed=True),
+    Track1Metric(relaxed=False),
+)
 
 parser: ArgumentParser = ArgumentParser()
 parser.add_argument('out', type=Path)
@@ -32,13 +36,10 @@ if __name__ == '__main__':
     args: Namespace = parser.parse_args()
 
     out: Path = args.out
-    matcher_name: str = args.matcher
-    metric_name: str = args.metric
-
-    matcher: Matcher = list(filter(lambda m: m.name == matcher_name, matchers))[0]
-    metric: Metric = list(filter(lambda m: m.name == metric_name, metrics))[0]
+    matcher: Matcher = next(filter(lambda m: m.name == args.matcher, matchers))
+    metric: Metric = next(filter(lambda m: m.name == args.metric, metrics))
 
     pipeline = Pipeline(matcher, metric)
     result = pipeline.train_evaluate(True, out)
 
-    print(result)
+    print(f"Score: {result} ({metric.name})")
