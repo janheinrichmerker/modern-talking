04fa09d825760d8d2211e6f5b9d0fc2c1d0d2217
Jan Heinrich Reimer <heinrich@reimer.family>
2021-05-24 22:10:49 +0200

    Fix duplicated code and code style

--- modern_talking/matchers/regression.py
+++ modern_talking/matchers/regression.py
@@ -122,7 +122,6 @@ class EnsemblePartOfSpeechMatcher(Matcher):
         features = self.encoder.transform([input_text]).toarray()
         # Predict label and probability with pretrained model.
         probability = self.model.predict_proba(features)
-        label = self.model.predict(features)[0]
         score = probability[0][1]  # get probability of class 1
         return score
 
@@ -155,32 +154,6 @@ class EnsembleVotingMatcher(Matcher):
         with path.open("wb") as file:
             dump((self.model, self.encoder), file)
 
-    @staticmethod
-    def get_texts(train_data: LabelledDataset) -> List[str]:
-        stemmer = SnowballStemmer("english")
-        train_texts: List[str] = []
-        for (arg_id, kp_id), label in train_data.labels.items():
-            arg = next(
-                arg for arg in train_data.arguments
-                if arg.id == arg_id
-            )
-            kp = next(
-                kp for kp in train_data.key_points
-                if kp.id == kp_id
-            )
-            arg_terms = [
-                stemmer.stem(term)
-                for term in word_tokenize(arg.text)
-            ]
-            kp_terms = [
-                stemmer.stem(term)
-                for term in word_tokenize(kp.text)
-            ]
-
-            text = " ".join(arg_terms) + " " + " ".join(kp_terms)
-            train_texts.append(text)
-        return train_texts
-
     def train(self, train_data: LabelledDataset, dev_data: LabelledDataset):
         """
         Encode training data with bag of words to get numeric features.
@@ -189,7 +162,7 @@ class EnsembleVotingMatcher(Matcher):
         """
         self.train_encoder(train_data)
 
-        train_features = self.encoder.transform(self.get_texts(train_data))
+        train_features = self.encoder.transform(get_texts(train_data))
         train_labels = array(list(train_data.labels.values()))
 
         log_regression = LogisticRegression(
@@ -208,7 +181,7 @@ class EnsembleVotingMatcher(Matcher):
         self.model.fit(train_features, train_labels)
 
     def train_encoder(self, train_data: LabelledDataset):
-        train_texts = self.get_texts(train_data)
+        train_texts = get_texts(train_data)
         self.encoder = CountVectorizer()  # token_pattern="^[a-zA-Z]{3,7}$")
         self.encoder.fit_transform(train_texts)
 
@@ -221,7 +194,6 @@ class EnsembleVotingMatcher(Matcher):
         features = self.encoder.transform([input_text]).toarray()
         # Predict label and probability with pretrained model.
         probability = self.model.predict_proba(features)
-        label = self.model.predict(features)[0]
         score = probability[0][1]  # get probability of class 1
         return score
 
@@ -232,6 +204,31 @@ class EnsembleVotingMatcher(Matcher):
             for kp in data.key_points
             if arg.topic == kp.topic and arg.stance == kp.stance
         }
+
+
+def get_texts(train_data: LabelledDataset) -> List[str]:
+    stemmer = SnowballStemmer("english")
+    train_texts: List[str] = []
+    for (arg_id, kp_id), label in train_data.labels.items():
+        arg = next(
+            arg for arg in train_data.arguments
+            if arg.id == arg_id
+        )
+        kp = next(
+            kp for kp in train_data.key_points
+            if kp.id == kp_id
+        )
+        arg_terms = [
+            stemmer.stem(term)
+            for term in word_tokenize(arg.text)
+        ]
+        kp_terms = [
+            stemmer.stem(term)
+            for term in word_tokenize(kp.text)
+        ]
+        text = " ".join(arg_terms) + " " + " ".join(kp_terms)
+        train_texts.append(text)
+    return train_texts
 
 
 class RegressionTfidfMatcher(Matcher):
@@ -258,35 +255,6 @@ class RegressionTfidfMatcher(Matcher):
         with path.open("wb") as file:
             dump((self.model, self.encoder), file)
 
-    @staticmethod
-    def get_texts(train_data: LabelledDataset) -> List[str]:
-        stemmer = SnowballStemmer("english")
-        train_texts: List[str] = []
-        for (arg_id, kp_id), label in train_data.labels.items():
-            arg = next(
-                arg for arg in train_data.arguments
-                if arg.id == arg_id
-            )
-            kp = next(
-                kp for kp in train_data.key_points
-                if kp.id == kp_id
-            )
-            topic_terms = [
-                stemmer.stem(term)
-                for term in word_tokenize(arg.topic)
-            ]
-            arg_terms = [
-                stemmer.stem(term)
-                for term in word_tokenize(arg.text)
-            ]
-            kp_terms = [
-                stemmer.stem(term)
-                for term in word_tokenize(kp.text)
-            ]
-            text = " ".join(arg_terms) + " " + " ".join(kp_terms)
-            train_texts.append(text)
-        return train_texts
-
     def train(self, train_data: LabelledDataset, dev_data: LabelledDataset):
         """
         Encode training data with bag of words to get numeric features.
@@ -295,7 +263,7 @@ class RegressionTfidfMatcher(Matcher):
         """
         self.train_encoder(train_data)
 
-        train_features = self.encoder.transform(self.get_texts(train_data))
+        train_features = self.encoder.transform(get_texts(train_data))
         train_labels = array(list(train_data.labels.values()))
 
         log_regression = LogisticRegression(
@@ -309,8 +277,8 @@ class RegressionTfidfMatcher(Matcher):
         self.model.fit(train_features, train_labels)
 
     def train_encoder(self, train_data: LabelledDataset):
-        train_texts = self.get_texts(train_data)
-        self.encoder = TfidfVectorizer()  # token_pattern="^[a-zA-Z]{3,7}$") # r'\w{2,}'
+        train_texts = get_texts(train_data)
+        self.encoder = TfidfVectorizer()
         self.encoder.fit_transform(train_texts)
 
     def get_match_probability(self, argument: Argument, key_point: KeyPoint):
@@ -362,37 +330,6 @@ class RegressionBagOfWordsMatcher(Matcher):
         with path.open("wb") as file:
             dump((self.model, self.encoder), file)
 
-    @staticmethod
-    def get_texts(train_data: LabelledDataset) -> List[str]:
-        stemmer = SnowballStemmer("english")
-        train_texts: List[str] = []
-        for (arg_id, kp_id), label in train_data.labels.items():
-            arg = next(
-                arg for arg in train_data.arguments
-                if arg.id == arg_id
-            )
-            kp = next(
-                kp for kp in train_data.key_points
-                if kp.id == kp_id
-            )
-            topic_terms = [
-                stemmer.stem(term)
-                for term in word_tokenize(arg.topic)
-            ]
-            arg_terms = [
-                stemmer.stem(term)
-                for term in word_tokenize(arg.text)
-            ]
-            kp_terms = [
-                stemmer.stem(term)
-                for term in word_tokenize(kp.text)
-            ]
-
-            # text = " ".join(topic_terms) + " ".join(arg_terms) + " " + " ".join(kp_terms)
-            text = " ".join(arg_terms) + " " + " ".join(kp_terms)
-            train_texts.append(text)
-        return train_texts
-
     def train(self, train_data: LabelledDataset, dev_data: LabelledDataset):
         """
         Encode training data with bag of words to get numeric features.
@@ -401,7 +338,7 @@ class RegressionBagOfWordsMatcher(Matcher):
         """
         self.train_encoder(train_data)
 
-        train_features = self.encoder.transform(self.get_texts(train_data))
+        train_features = self.encoder.transform(get_texts(train_data))
         train_labels = array(list(train_data.labels.values()))
 
         log_regression = LogisticRegression(
@@ -415,8 +352,8 @@ class RegressionBagOfWordsMatcher(Matcher):
         self.model.fit(train_features, train_labels)
 
     def train_encoder(self, train_data: LabelledDataset):
-        train_texts = self.get_texts(train_data)
-        self.encoder = CountVectorizer()  # token_pattern="^[a-zA-Z]{3,7}$")
+        train_texts = get_texts(train_data)
+        self.encoder = CountVectorizer()
         self.encoder.fit_transform(train_texts)
 
     def get_match_probability(self, argument: Argument, key_point: KeyPoint):
@@ -428,12 +365,7 @@ class RegressionBagOfWordsMatcher(Matcher):
         features = self.encoder.transform([input_text]).toarray()
         # Predict label and probability with pretrained model.
         probability = self.model.predict_proba(features)
-        label = self.model.predict(features)[0]
         score = probability[0][1]  # get probability of class 1
-        # if label[0] == 1.0:
-        #    score = probability[0][1]
-        # else:
-        #    score = 0
         return score
 
     def predict(self, data: Dataset) -> Labels:
