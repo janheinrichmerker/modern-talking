1109640f7da9eeb4bf424fb4947c7139e4be7f99
Jan Heinrich Reimer <heinrich@reimer.family>
2021-06-08 18:21:42 +0200

    Add text layers

---
+++ modern_talking/matchers/layers.py
@@ -0,0 +1,41 @@
+from typing import List
+
+from tensorflow.python.data import Dataset
+from tensorflow.python.keras.initializers.initializers_v2 import Constant
+from tensorflow.python.keras.layers import Embedding, TextVectorization
+
+from modern_talking.data.glove import get_glove_embedding_matrix
+
+
+def text_vectorization_layer(
+        texts: List[str],
+        max_tokens: int = 100_000
+) -> TextVectorization:
+    """
+    Create a text vectorization layer with the
+    for the most frequent tokens in the given texts.
+    """
+    layer = TextVectorization(max_tokens)
+    text_dataset = Dataset.from_tensor_slices(texts)
+    layer.adapt(text_dataset)
+    return layer
+
+
+def glove_embedding_layer(
+        vectorization_layer: TextVectorization
+) -> Embedding:
+    """
+    Create a GloVe word embedding layer
+    to be used after the vectorization layer.
+    Note that GloVe embeddings have to be downloaded first.
+    """
+    vocabulary = vectorization_layer.get_vocabulary()
+    initial_matrix = get_glove_embedding_matrix(vocabulary)
+    dimension = initial_matrix.shape[1]
+    layer = Embedding(
+        len(vocabulary) + 2,
+        dimension,
+        embeddings_initializer=Constant(initial_matrix),
+        trainable=False,
+    )
+    return layer
