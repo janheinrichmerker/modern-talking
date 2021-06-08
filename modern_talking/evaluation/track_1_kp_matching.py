3e169a2cf492f29cf0faa594e2cf2e01fb954488
Jan Heinrich Reimer <heinrich@reimer.family>
2021-06-08 15:25:57 +0200

    Update shared task evaluation code

--- modern_talking/evaluation/track_1_kp_matching.py
+++ modern_talking/evaluation/track_1_kp_matching.py
@@ -1,35 +1,31 @@
+# From the task organizers:
+# https://github.com/IBM/KPA_2021_shared_task/blob/eeaaf65fdcd2777be2485282786a917f6965c38b/code/track_1_kp_matching.py
+
 import sys
+import pandas as pd
+from sklearn.metrics import precision_recall_curve, average_precision_score, precision_score
+from matplotlib import pyplot
+import numpy as np
 import os
 import json
 
-import pandas as pd
-from sklearn.metrics import average_precision_score
-import numpy as np
 
 
 def get_ap(df, label_column, top_percentile=0.5):
-    top = int(len(df) * top_percentile)
+    top = int(len(df)*top_percentile)
     df = df.sort_values('score', ascending=False).head(top)
-    return average_precision_score(
-        y_true=df[label_column],
-        y_score=df["score"]
-    )
-
+    # after selecting top percentile candidates, we set the score for the dummy kp to 1, to prevent it from increasing the precision.
+    df.loc[df['key_point_id'] == "dummy_id", 'score'] = 0.99
+    return average_precision_score(y_true=df[label_column], y_score=df["score"])
 
 def calc_mean_average_precision(df, label_column):
-    precisions = [
-        get_ap(group, label_column)
-        for _, group
-        in df.groupby(["topic", "stance"])
-    ]
+    precisions = [get_ap(group, label_column) for _, group in df.groupby(["topic", "stance"])]
     return np.mean(precisions)
 
-
 def evaluate_predictions(merged_df):
-    map_strict = calc_mean_average_precision(merged_df, "label_strict")
-    map_relaxed = calc_mean_average_precision(merged_df, "label_relaxed")
-    print(f"mAP strict= {map_strict} ; mAP relaxed = {map_relaxed}")
-
+    mAP_strict = calc_mean_average_precision(merged_df, "label_strict")
+    mAP_relaxed = calc_mean_average_precision(merged_df, "label_relaxed")
+    print(f"mAP strict= {mAP_strict} ; mAP relaxed = {mAP_relaxed}")
 
 def load_kpm_data(gold_data_dir, subset):
     arguments_file = os.path.join(gold_data_dir, f"arguments_{subset}.csv")
@@ -46,18 +42,15 @@ def load_kpm_data(gold_data_dir, subset):
 def get_predictions(predictions_file, labels_df, arg_df):
     arg_df = arg_df[["arg_id", "topic", "stance"]]
     predictions_df = load_predictions(predictions_file)
-    # make sure each arg_id has a prediction
-    predictions_df = pd.merge(arg_df, predictions_df,
-                              how="left", on="arg_id")
+    #make sure each arg_id has a prediction
+    predictions_df = pd.merge(arg_df, predictions_df, how="left", on="arg_id")
 
-    # handle arguements with no matching key point
-    predictions_df["key_point_id"] = predictions_df["key_point_id"] \
-        .fillna("dummy_id")
+    #handle arguements with no matching key point
+    predictions_df["key_point_id"] = predictions_df["key_point_id"].fillna("dummy_id")
     predictions_df["score"] = predictions_df["score"].fillna(0)
 
-    # merge each argument with the gold labels
-    merged_df = pd.merge(predictions_df, labels_df,
-                         how="left", on=["arg_id", "key_point_id"])
+    #merge each argument with the gold labels
+    merged_df = pd.merge(predictions_df, labels_df, how="left", on=["arg_id", "key_point_id"])
 
     merged_df.loc[merged_df['key_point_id'] == "dummy_id", 'label'] = 0
     merged_df["label_strict"] = merged_df["label"].fillna(0)
@@ -65,12 +58,12 @@ def get_predictions(predictions_file, labels_df, arg_df):
     return merged_df
 
 
+"""
+this method chooses the best key point for each argument
+and generates a dataframe with the matches and scores
+"""
 def load_predictions(predictions_dir):
-    """
-    this method chooses the best key point for each argument
-    and generates a dataframe with the matches and scores
-    """
-    arg = []
+    arg =[]
     kp = []
     scores = []
     with open(predictions_dir, "r") as f_in:
@@ -80,23 +73,17 @@ def load_predictions(predictions_dir):
             arg.append(arg_id)
             kp.append(best_kp[0])
             scores.append(best_kp[1])
-        return pd.DataFrame({
-            "arg_id": arg,
-            "key_point_id": kp,
-            "score": scores
-        })
-
-
-def evaluate(gold_data_dir, predictions_file):
-    arg_df, kp_df, labels_df = load_kpm_data(gold_data_dir, subset="dev")
-
-    merged_df = get_predictions(predictions_file, labels_df, arg_df)
-    evaluate_predictions(merged_df)
-
+        print(f"loaded predictions for {len(arg)} arguments")
+        return pd.DataFrame({"arg_id" : arg, "key_point_id": kp, "score": scores})
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("You must specify two parameters for this scripts: input data "
-              "directory and the predictions file")
+        print("You must specify two parameters for this scripts: input data directory and the predictions file")
     else:
-        evaluate(sys.argv[1], sys.argv[2])
+        gold_data_dir = sys.argv[1]
+        predictions_file = sys.argv[2]
+
+        arg_df, kp_df, labels_df = load_kpm_data(gold_data_dir, subset="dev")
+
+        merged_df = get_predictions(predictions_file, labels_df, arg_df)
+        evaluate_predictions(merged_df)
