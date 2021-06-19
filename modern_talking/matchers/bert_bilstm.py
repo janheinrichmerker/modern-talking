bad2259a1aafb635efa5a8ef59899618f56c73d2
Jan Heinrich Reimer <heinrich@reimer.family>
2021-06-20 01:53:54 +0200

    Fix BERT BiLSTM model

--- modern_talking/matchers/bert_bilstm.py
+++ modern_talking/matchers/bert_bilstm.py
@@ -3,15 +3,15 @@ from typing import Tuple, List
 
 from keras import Input, Model
 from keras.activations import relu, softmax
+from keras.callbacks import ModelCheckpoint
 from keras.layers import Dense, Dropout, Concatenate, Layer, Subtract, \
     GlobalMaxPooling1D, GlobalAveragePooling1D, Bidirectional, LSTM, \
     SpatialDropout1D
 from keras.losses import CategoricalCrossentropy
 from keras.metrics import Precision, Recall
 from keras.optimizer_v2.adam import Adam
-from numpy import ndarray, array
-from tensorflow import data
-from tensorflow import string
+from numpy import ndarray
+from tensorflow import data, int32
 from transformers import TFPreTrainedModel, PretrainedConfig, \
     PreTrainedTokenizerFast, AutoConfig, AutoTokenizer, TFAutoModel, \
     BatchEncoding
@@ -32,54 +32,80 @@ class MergeType(Enum):
     subtract = 2
 
 
+class PretrainedTokenizer(Layer):
+    tokenizer: PreTrainedTokenizerFast
+    config: PretrainedConfig
+
+    def __init__(
+            self,
+            tokenizer: PreTrainedTokenizerFast,
+            config: PretrainedConfig,
+            **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.tokenizer = tokenizer
+        self.config = config
+
+    def call(self, inputs, **kwargs):
+        return self.tokenizer(
+            inputs.numpy().tolist(),
+            padding=True,
+            truncation=True,
+            max_length=self.config.max_length,
+            return_tensors="tf",
+        )
+
+
 def create_model(
         pretrained_model: TFPreTrainedModel,
-        tokenizer: PreTrainedTokenizerFast,
-        config: PretrainedConfig,
         embedding_dropout: float = 0.2,
         bilstm_units: int = 64,
         memory_dropout: float = 0.2,
         merge_memories: MergeType = MergeType.subtract
 ) -> Model:
     # Specify model inputs.
-    argument_text = Input(
-        name="argument_text",
-        shape=(1,),
-        dtype=string
+    argument_input_ids = Input(
+        name="argument_input_ids",
+        shape=(pretrained_model.config.max_length,),
+        dtype=int32,
     )
-    key_point_text = Input(
-        name="key_point_text",
-        shape=(1,),
-        dtype=string
+    argument_attention_mask = Input(
+        name="argument_attention_mask",
+        shape=(pretrained_model.config.max_length,),
+        dtype=int32,
     )
-
-    # Tokenize using pretrained tokenizer (e.g., WordPiece)
-    argument_encoding: BatchEncoding = tokenizer(
-        argument_text,
-        padding=True,
-        truncation=True,
-        max_length=config.max_length,
-        return_tensors="tf",
+    argument_token_type_ids = Input(
+        name="argument_token_type_ids",
+        shape=(pretrained_model.config.max_length,),
+        dtype=int32,
     )
-    key_point_encoding: BatchEncoding = tokenizer(
-        key_point_text,
-        padding=True,
-        truncation=True,
-        max_length=config.max_length,
-        return_tensors="tf",
+    key_point_input_ids = Input(
+        name="key_point_input_ids",
+        shape=(pretrained_model.config.max_length,),
+        dtype=int32,
+    )
+    key_point_attention_mask = Input(
+        name="key_point_attention_mask",
+        shape=(pretrained_model.config.max_length,),
+        dtype=int32,
+    )
+    key_point_token_type_ids = Input(
+        name="key_point_token_type_ids",
+        shape=(pretrained_model.config.max_length,),
+        dtype=int32,
     )
 
     # Embed with pretrained transformer model.
     argument_embed: TFBaseModelOutput = pretrained_model(
-        argument_encoding["input_ids"],
-        attention_mask=argument_encoding["attention_masks"],
-        # token_type_ids=argument_encoding["token_type_ids"],
+        argument_input_ids,
+        attention_mask=argument_attention_mask,
+        # token_type_ids=argument_token_type_ids,
     )
     argument_embed = SpatialDropout1D(embedding_dropout)(argument_embed)
     key_point_embed: TFBaseModelOutput = pretrained_model(
-        key_point_encoding["input_ids"],
-        attention_mask=key_point_encoding["attention_masks"],
-        # token_type_ids=key_point_encoding["token_type_ids"],
+        key_point_input_ids,
+        attention_mask=key_point_attention_mask,
+        # token_type_ids=key_point_token_type_ids,
     )
     key_point_embed = SpatialDropout1D(embedding_dropout)(key_point_embed)
 
@@ -119,41 +145,93 @@ def create_model(
 
     # Define model.
     pretrained_model = Model(
-        inputs=[argument_text, key_point_text],
+        inputs=[
+            argument_input_ids,
+            argument_attention_mask,
+            argument_token_type_ids,
+            key_point_input_ids,
+            key_point_attention_mask,
+            key_point_token_type_ids,
+        ],
         outputs=output
     )
     return pretrained_model
 
 
+def _prepare_encodings(
+        texts: List[str],
+        tokenizer: PreTrainedTokenizerFast,
+        config: PretrainedConfig
+) -> BatchEncoding:
+    # Tokenize using pretrained tokenizer (e.g., WordPiece)
+    encodings: BatchEncoding = tokenizer(
+        texts,
+        padding=True,
+        truncation=True,
+        max_length=config.max_length,
+        return_attention_mask=True,
+        return_token_type_ids=True,
+        return_tensors="tf",
+    )
+    return encodings
+
+
 def _prepare_unlabelled_data(
-        dataset: UnlabelledDataset
+        dataset: UnlabelledDataset,
+        tokenizer: PreTrainedTokenizerFast,
+        config: PretrainedConfig
 ) -> Tuple[Dataset, List[ArgumentKeyPointIdPair]]:
-    pairs = [(arg, kp) for arg in dataset.arguments for kp in
-             dataset.key_points]
+    pairs = [
+        (arg, kp)
+        for arg in dataset.arguments
+        for kp in dataset.key_points
+        if arg.topic == kp.topic and arg.stance == kp.stance
+    ]
     ids = [(arg.id, kp.id) for arg, kp in pairs]
     arg_texts = [arg.text for arg, kp in pairs]
     kp_texts = [kp.text for arg, kp in pairs]
+    arg_encodings = _prepare_encodings(arg_texts, tokenizer, config)
+    kp_encodings = _prepare_encodings(kp_texts, tokenizer, config)
     dataset = Dataset.from_tensor_slices((
         {
-            "argument_text": array(arg_texts),
-            "key_point_text": array(kp_texts)
-        },
+            "argument_input_ids": arg_encodings["input_ids"],
+            "argument_attention_mask": arg_encodings["attention_mask"],
+            "argument_token_type_ids": arg_encodings["token_type_ids"],
+            "key_point_input_ids": kp_encodings["input_ids"],
+            "key_point_attention_mask": kp_encodings["attention_mask"],
+            "key_point_token_type_ids": kp_encodings["token_type_ids"],
+        }
     ))
     return dataset, ids
 
 
-def _prepare_labelled_data(dataset: LabelledDataset) -> Dataset:
-    pairs = [(arg, kp) for arg in dataset.arguments for kp in
-             dataset.key_points]
+def _prepare_labelled_data(
+        dataset: LabelledDataset,
+        tokenizer: PreTrainedTokenizerFast,
+        config: PretrainedConfig
+) -> Dataset:
+    pairs = [
+        (arg, kp)
+        for arg in dataset.arguments
+        for kp in dataset.key_points
+    ]
     arg_texts = [arg.text for arg, kp in pairs]
     kp_texts = [kp.text for arg, kp in pairs]
+    arg_encodings = _prepare_encodings(arg_texts, tokenizer, config)
+    print(arg_encodings.keys())
+    kp_encodings = _prepare_encodings(kp_texts, tokenizer, config)
+    print(kp_encodings.keys())
     labels = encode_labels(
         dataset.labels.get((arg.id, kp.id)) for arg, kp in pairs
     )
     dataset = Dataset.from_tensor_slices((
         {
-            "argument_text": array(arg_texts),
-            "key_point_text": array(kp_texts),
+            "argument_input_ids": arg_encodings["input_ids"],
+            "argument_attention_mask": arg_encodings["attention_mask"],
+            "argument_token_type_ids": arg_encodings["token_type_ids"],
+            "key_point_input_ids": kp_encodings["input_ids"],
+            "key_point_attention_mask": kp_encodings["attention_mask"],
+            "key_point_token_type_ids": kp_encodings["token_type_ids"],
         },
         labels,
     ))
@@ -167,7 +245,7 @@ class BertBilstmMatcher(Matcher):
     """
 
     batch_size = 32
-    epochs = 1  # 0
+    epochs = 1
 
     pretrained_model_name: str
     embedding_dropout: float
@@ -223,35 +301,53 @@ class BertBilstmMatcher(Matcher):
 
     def train(self, train_data: LabelledDataset, dev_data: LabelledDataset):
         # Load and prepare datasets as tensors.
-        train_dataset = _prepare_labelled_data(train_data)
+        print("\tLoad and prepare datasets for model.")
+        train_dataset = _prepare_labelled_data(
+            train_data,
+            self.tokenizer,
+            self.config
+        )
         train_dataset = train_dataset.batch(self.batch_size)
-        dev_dataset = _prepare_labelled_data(dev_data)
+        dev_dataset = _prepare_labelled_data(
+            dev_data,
+            self.tokenizer,
+            self.config
+        )
         dev_dataset = dev_dataset.batch(self.batch_size)
 
         # Build model.
+        print("\tBuild and compile model.")
         self.model = create_model(
             self.pretrained_model,
-            self.tokenizer,
-            self.config,
             self.embedding_dropout,
             self.bilstm_units,
             self.memory_dropout,
             self.merge_memories
         )
 
+        # Setup checkpointing for model weights
+        checkpoint = ModelCheckpoint(
+            "weights-improvement-{epoch:02d}-{val_precision:.3f}.hdf5",
+            monitor='val_precision',
+            save_best_only=True,
+            mode='max'
+        )
+
         # Compile model.
         self.model.compile(
-            optimizer=Adam(),
+            optimizer=Adam(2e-5),
             loss=CategoricalCrossentropy(),
             metrics=[Precision(), Recall()],
         )
         self.model.summary()
 
         # Train model.
+        print("\tTrain compiled model.")
         self.model.fit(
             train_dataset,
             validation_data=dev_dataset,
             epochs=self.epochs,
+            callbacks=[checkpoint],
         )
 
         # Evaluate model on dev set.
@@ -259,16 +355,22 @@ class BertBilstmMatcher(Matcher):
 
     def predict(self, test_data: UnlabelledDataset) -> Labels:
         # Load and prepare datasets as tensors.
-        test_dataset, tesst_ids = _prepare_unlabelled_data(test_data)
+        print("\tLoad and prepare datasets for model.")
+        test_dataset, test_ids = _prepare_unlabelled_data(
+            test_data,
+            self.tokenizer,
+            self.config
+        )
         test_dataset = test_dataset.batch(self.batch_size)
 
         # Predict and decode labels (one-cold).
+        print("\tPredict and decode labels.")
         predictions: ndarray = self.model.predict(test_dataset)
         labels = decode_labels(predictions)
 
         # Return predictions.
         return {
             arg_kp_id: label
-            for arg_kp_id, label in zip(tesst_ids, labels)
+            for arg_kp_id, label in zip(test_ids, labels)
             if label is not None
         }
