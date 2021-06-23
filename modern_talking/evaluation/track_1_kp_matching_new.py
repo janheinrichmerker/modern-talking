0dec9ab17dd9302dc23ea828d0843b0345379aa0
Jan Heinrich Reimer <heinrich@reimer.family>
2021-06-23 18:24:04 +0200

    Improve evaluation script documentation

--- modern_talking/evaluation/track_1_kp_matching_new.py
+++ modern_talking/evaluation/track_1_kp_matching_new.py
@@ -8,7 +8,12 @@ from pandas import DataFrame, read_csv
 from sklearn.metrics import average_precision_score
 
 
-def get_ap(df: DataFrame, label_column: str, top_percentile: float = 0.5):
+def calculate_average_precision(df: DataFrame, label_column: str,
+                                top_percentile: float = 0.5):
+    """
+    Calculate average precision score for top-scores in the data frame
+    with respect to relaxed or strict ground truth labels.
+    """
     top = int(len(df) * top_percentile)
     df = df.sort_values("score", ascending=False).head(top)
     return average_precision_score(
@@ -17,24 +22,39 @@ def get_ap(df: DataFrame, label_column: str, top_percentile: float = 0.5):
     )
 
 
-def calc_mean_average_precision(df: DataFrame, label_column: str):
+def calculate_mean_average_precision(df: DataFrame, label_column: str):
+    """
+    Calculate mean average precision score for top-scores
+    with respect to relaxed or strict ground truth labels
+    across argument key point pairs of same topic and stance.
+    """
     precisions = [
-        get_ap(group, label_column)
+        calculate_average_precision(group, label_column)
         for _, group in df.groupby(["topic", "stance"])
     ]
     return mean(precisions)
 
 
 def evaluate_predictions(merged_df: DataFrame):
-    map_strict = calc_mean_average_precision(merged_df, "label_strict")
-    map_relaxed = calc_mean_average_precision(merged_df, "label_relaxed")
+    """
+    Calculate mean average precision scores
+    for relaxed and strict ground truth labels
+    and print evaluation scores to console.
+    """
+    map_strict = calculate_mean_average_precision(merged_df, "label_strict")
+    map_relaxed = calculate_mean_average_precision(merged_df, "label_relaxed")
     print(f"mAP strict= {map_strict} ; mAP relaxed = {map_relaxed}")
 
 
 def load_kpm_data(
-        gold_data_dir:
-        Path, subset: str
+        gold_data_dir: Path,
+        subset: str
 ) -> Tuple[DataFrame, DataFrame, DataFrame]:
+    """
+    Load arguments, key points, and ground truth labels from
+    the data directory.
+    """
+
     arguments_file = gold_data_dir / f"arguments_{subset}.csv"
     key_points_file = gold_data_dir / f"key_points_{subset}.csv"
     labels_file = gold_data_dir / f"labels_{subset}.csv"
@@ -46,12 +66,41 @@ def load_kpm_data(
     return arguments_df, key_points_df, labels_file_df
 
 
-def merge_labels_predictions(
+def load_predictions(predictions_file: Path) -> DataFrame:
+    """
+    Load data frame with argument key point matches and scores.
+    """
+    args: List[str] = []
+    kps: List[str] = []
+    scores: List[float] = []
+    with predictions_file.open("r") as file_in:
+        predictions = load(file_in)
+    for arg_id, kp_scores in predictions.items():
+        for kp_id, score in kp_scores.items():
+            args.append(arg_id)
+            kps.append(kp_id)
+            scores.append(score)
+    print(f"Loaded {len(args)} predictions "
+          f"for {len(predictions.items())} arguments.")
+    return DataFrame({
+        "arg_id": args,
+        "key_point_id": kps,
+        "score": scores
+    })
+
+
+def merge_labels_with_predictions(
         arg_df: DataFrame,
         kp_df: DataFrame,
         labels_df: DataFrame,
         predictions_df: DataFrame
 ) -> DataFrame:
+    """
+    Merge ground truth labels and predicted match scores
+    for all argument key point pairs.
+    Missing values are filled in as described in the task description.
+    """
+
     # Remove text columns. These are not needed for evaluation.
     arg_df = arg_df.drop(columns=["argument"])
     kp_df = kp_df.drop(columns=["key_point"])
@@ -96,29 +145,6 @@ def merge_labels_predictions(
     return merged_df
 
 
-def load_predictions(predictions_file: Path) -> DataFrame:
-    """
-    Generate a data frame with argument key point matches and scores.
-    """
-    args: List[str] = []
-    kps: List[str] = []
-    scores: List[float] = []
-    with predictions_file.open("r") as file_in:
-        predictions = load(file_in)
-    for arg_id, kp_scores in predictions.items():
-        for kp_id, score in kp_scores.items():
-            args.append(arg_id)
-            kps.append(kp_id)
-            scores.append(score)
-    print(f"Loaded {len(args)} predictions "
-          f"for {len(predictions.items())} arguments.")
-    return DataFrame({
-        "arg_id": args,
-        "key_point_id": kps,
-        "score": scores
-    })
-
-
 def main():
     if len(argv) != 3:
         print("You must specify two parameters for this scripts: "
@@ -130,7 +156,7 @@ def main():
 
     arg_df, kp_df, labels_df = load_kpm_data(gold_data_dir, subset="dev")
     predictions_df = load_predictions(predictions_file)
-    merged_df = merge_labels_predictions(
+    merged_df = merge_labels_with_predictions(
         arg_df,
         kp_df,
         labels_df,
