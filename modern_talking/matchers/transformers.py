41a500ddfe4328096e0ea2248fe70f57f03337ee
Jan Heinrich Reimer <heinrich@reimer.family>
2021-06-25 17:05:38 +0200

    Improve transformers model

--- modern_talking/matchers/transformers.py
+++ modern_talking/matchers/transformers.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 from imblearn.over_sampling import RandomOverSampler
 from nlpaug.augmenter.word import SynonymAug, AntonymAug, RandomWordAug
@@ -10,7 +10,8 @@ from simpletransformers.classification import ClassificationModel
 from simpletransformers.config.model_args import ClassificationArgs
 from torch.cuda import is_available as is_cuda_available
 
-from modern_talking.matchers import Matcher, UnknownLabelPolicy
+from modern_talking.matchers import Matcher, LabelPolicy
+from modern_talking.matchers.utils import describe_model_configuration
 from modern_talking.model import Dataset, Labels, LabelledDataset, \
     ArgumentKeyPointPair
 
@@ -18,14 +19,18 @@ from modern_talking.model import Dataset, Labels, LabelledDataset, \
 class TransformersMatcher(Matcher):
     model_type: str
     model_name: str
+    max_sequence_length: int
     augment: int
-    unknown_label_policy: UnknownLabelPolicy
+    label_policy: LabelPolicy
     over_sample: bool
     shuffle: bool
     batch_size: int
     epochs: int
     early_stopping: bool
-    seed: int = 1234
+    learning_rate: float
+    warmup_ratio: float
+    weight_decay: float
+    seed: int
 
     model: ClassificationModel
 
@@ -33,46 +38,63 @@ class TransformersMatcher(Matcher):
             self,
             model_type: str,
             model_name: str,
+            max_sequence_length: int = 128,
             augment: int = 0,
-            unknown_label_policy: UnknownLabelPolicy = UnknownLabelPolicy.skip,
+            label_policy: LabelPolicy = LabelPolicy.skip,
             over_sample: bool = False,
             shuffle: bool = True,
             batch_size: int = 16,
             epochs: int = 1,
             early_stopping: bool = False,
+            learning_rate: float = 4e-5,
+            warmup_ratio: float = 0.06,
+            weight_decay: float = 0.0,
+            seed: int = 1234
     ):
         self.model_type = model_type
         self.model_name = model_name
+        self.max_sequence_length = max_sequence_length
         self.augment = augment
-        self.unknown_label_policy = unknown_label_policy
+        self.label_policy = label_policy
         self.over_sample = over_sample
         self.shuffle = shuffle
         self.batch_size = batch_size
         self.epochs = epochs
         self.early_stopping = early_stopping
+        self.learning_rate = learning_rate
+        self.warmup_ratio = warmup_ratio
+        self.weight_decay = weight_decay
+        self.seed = seed
 
     @property
     def slug(self) -> str:
         augment_suffix = f"-augment-{self.augment}" \
-            if self.augment >= 2 else ""
-        unknown_label_policy_suffix = "-relaxed" \
-            if self.unknown_label_policy == UnknownLabelPolicy.relaxed \
+            if self.augment > 0 else ""
+        label_policy_suffix = "-relaxed" \
+            if self.label_policy == LabelPolicy.relaxed \
             else "-strict" \
-            if self.unknown_label_policy == UnknownLabelPolicy.strict \
+            if self.label_policy == LabelPolicy.strict \
             else ""
         over_sample_suffix = "-over-sample" if self.over_sample else ""
         shuffle_suffix = "-shuffle" if self.shuffle else ""
+        weight_decay_suffix = f"-weight-decay-{self.weight_decay}" \
+            if self.weight_decay > 0 else ""
+        warmup_suffix = f"-warmup-{self.warmup_ratio}" \
+            if self.warmup_ratio > 0 else ""
         early_stopping_suffix = "-early-stopping" \
             if self.early_stopping else ""
         return f"transformers" \
                f"-{self.model_type}" \
                f"-{self.model_name.replace('/', '-')}" \
                f"{augment_suffix}" \
-               f"{unknown_label_policy_suffix}" \
+               f"{label_policy_suffix}" \
                f"{over_sample_suffix}" \
                f"{shuffle_suffix}" \
                f"-batch-{self.batch_size}" \
                f"-epochs-{self.epochs}" \
+               f"-learn-{self.learning_rate}" \
+               f"{weight_decay_suffix}" \
+               f"{warmup_suffix}" \
                f"{early_stopping_suffix}"
 
     @property
@@ -94,51 +116,44 @@ class TransformersMatcher(Matcher):
 
     @property
     def description(self) -> Optional[str]:
-        augment_suffix = f"\nAugment arguments and key points " \
-                         f"{self.augment} times" if self.augment >= 2 else ""
-        unknown_label_policy_suffix = "\nFill missing training labels " \
-                                      "with 1." \
-            if self.unknown_label_policy == UnknownLabelPolicy.relaxed \
-            else "\nFill missing training labels with 0." \
-            if self.unknown_label_policy == UnknownLabelPolicy.strict \
-            else "\nSkip missing training labels."
-        over_sample_suffix = "\nOversample training data randomly."\
-            if self.over_sample else ""
-        shuffle_suffix = "\nShuffle training data." if self.shuffle else ""
-        early_stopping_suffix = "\nStop early when loss validation set " \
-                                "does not decrease for 5 epochs." \
-            if self.early_stopping else ""
-        return f"Classify matches with {self.model_name} " \
-               f"Huggingface Transformers model (type {self.model_type})." \
-               f"{augment_suffix}" \
-               f"{unknown_label_policy_suffix}" \
-               f"{over_sample_suffix}" \
-               f"{shuffle_suffix}" \
-               f"\nFine-tune with batch size {self.batch_size} " \
-               f"for {self.epochs} epochs." \
-               f"{early_stopping_suffix}"
+        config = describe_model_configuration(
+            self.model,
+            self.augment,
+            self.label_policy,
+            self.over_sample,
+            self.shuffle,
+        )
+        return f"Classify matches with '{self.model_name}' " \
+               f"Huggingface Transformers model " \
+               f"(type '{self.model_type}').\n\n" \
+               f"{config}"
 
     def prepare(self) -> None:
         # Configure model.
-        args = ClassificationArgs()
-        args.overwrite_output_dir = True
-        args.regression = True
-        args.do_lower_case = "uncased" in self.model_name
-        args.train_batch_size = self.batch_size
-        args.eval_batch_size = self.batch_size
-        args.num_train_epochs = self.epochs
-        args.evaluate_during_training = True
-        args.evaluate_during_training_steps = 1000
-        args.use_early_stopping = self.early_stopping
-        args.early_stopping_patience = 5
-        args.manual_seed = self.seed
+        args = ClassificationArgs(
+            model_type=self.model_type,
+            model_name=self.model_name,
+            overwrite_output_dir=True,
+            regression=True,
+            max_seq_length=self.max_sequence_length,
+            do_lower_case="uncased" in self.model_name,
+            train_batch_size=self.batch_size,
+            eval_batch_size=self.batch_size,
+            num_train_epochs=self.epochs,
+            use_early_stopping=self.early_stopping,
+            early_stopping_patience=5,
+            learning_rate=self.learning_rate,
+            warmup_ratio=self.warmup_ratio,
+            weight_decay=self.weight_decay,
+            manual_seed=self.seed,
+        )
 
         # Load pretrained model.
         self.model = ClassificationModel(
-            model_type=self.model_type,
-            model_name=self.model_name,
-            num_labels=1,
+            model_type=args.model_type,
+            model_name=args.model_name,
             args=args,
+            num_labels=1,
             use_cuda=is_cuda_available(),
         )
 
@@ -158,39 +173,25 @@ class TransformersMatcher(Matcher):
             dev_data: LabelledDataset,
             cache_path: Path,
     ):
-        # Load data.
-        train_df = _text_pair_df(
-            train_data,
-            self.augment,
-            self.unknown_label_policy,
-            self.over_sample
-        )
-        if self.shuffle:
-            train_df = train_df.sample(frac=1, random_state=self.seed)
-        dev_df = _text_pair_df(
-            dev_data,
-            self.augment,
-            self.unknown_label_policy,
-        )
-        if self.shuffle:
-            dev_df = dev_df.sample(frac=1, random_state=self.seed)
-
         # Configure model cache/checkpoint directories.
         self.model.args.cache_dir = str(cache_path / "cache")
         self.model.args.output_dir = str(cache_path / "out")
         self.model.args.tensorboard_dir = str(cache_path / "runs")
         self.model.args.best_model_dir = str(cache_path / "best_model")
 
+        # Load data.
+        train_df = self._prepare_train_data(train_data)
+        dev_df = self._prepare_dev_data(dev_data)
+
         # Train model.
         self.model.train_model(train_df, eval_df=dev_df)
 
     def predict(self, data: Dataset) -> Labels:
         # Load data.
-        pairs = _arg_kp_pairs(data)
-        inputs = [[arg.text, kp.text] for arg, kp in pairs]
+        pairs, texts = self._prepare_test_data(data)
 
         # Predict labels.
-        predictions, _ = self.model.predict(inputs)
+        predictions, _ = self.model.predict(texts)
 
         # Return predictions.
         return {
@@ -206,76 +207,113 @@ class TransformersMatcher(Matcher):
             self.model = ClassificationModel(
                 model_type=self.model_type,
                 model_name=model_path,
+                use_cuda=is_cuda_available(),
             )
             return True
 
     def save_model(self, path: Path):
-        self.model.save_model(path / "model")
+        self.model.save_model(path / "model", model=self.model.model)
 
-
-def _text_pair_df(
-        data: LabelledDataset,
-        augment: int,
-        unknown_label_policy: UnknownLabelPolicy,
-        over_sample: bool = False,
-) -> DataFrame:
-    pairs = _arg_kp_pairs(data)
-    if unknown_label_policy == UnknownLabelPolicy.skip:
+    def _prepare_train_data(self, data: LabelledDataset) -> DataFrame:
         pairs = [
             (arg, kp)
-            for (arg, kp) in pairs
-            if (arg.id, kp.id) in data.labels.keys()
+            for arg in data.arguments
+            for kp in data.key_points
+            if (arg.topic == kp.topic and arg.stance == kp.stance
+                and (self.label_policy != LabelPolicy.skip
+                     or (arg.id, kp.id) in data.labels.keys()))
         ]
-    arg_texts: List[str] = []
-    kp_texts: List[str] = []
-    labels: List[float] = []
-    augmenter: Optional[Pipeline] = None
-    if augment >= 2:
-        augmenter = Sometimes([
-            SynonymAug("wordnet"),
-            AntonymAug("wordnet"),
-            RandomWordAug(action="swap"),
-            RandomWordAug(action="delete"),
-        ])
-    for arg, kp in pairs:
-        current_arg_texts = [arg.text]
-        current_kp_texts = [kp.text]
-        if augmenter is not None:
-            current_arg_texts.extend(augmenter.augment(arg.text, n=augment))
-            current_kp_texts.extend(augmenter.augment(kp.text, n=augment))
-        for arg_text, kp_text in zip(current_arg_texts, current_kp_texts):
-            label: float
-            if (arg.id, kp.id) not in data.labels.keys():
-                if unknown_label_policy == UnknownLabelPolicy.strict:
-                    label = 0
-                elif unknown_label_policy == UnknownLabelPolicy.relaxed:
-                    label = 1
+        arg_texts: List[str] = []
+        kp_texts: List[str] = []
+        labels: List[float] = []
+
+        augmenter: Optional[Pipeline] = None
+        if self.augment > 0:
+            augmenter = Sometimes([
+                SynonymAug("wordnet"),
+                AntonymAug("wordnet"),
+                RandomWordAug(action="swap"),
+                RandomWordAug(action="delete"),
+            ])
+
+        for arg, kp in pairs:
+            current_arg_texts = [arg.text]
+            current_kp_texts = [kp.text]
+
+            if augmenter is not None:
+                augmented_arg = augmenter.augment(arg.text, n=self.augment)
+                if isinstance(augmented_arg, List):
+                    current_arg_texts.extend(augmented_arg)
                 else:
-                    raise Exception("Broken unknown label policy.")
-            else:
-                label = data.labels[arg.id, kp.id]
+                    current_arg_texts.append(augmented_arg)
+                augmented_kp = augmenter.augment(kp.text, n=self.augment)
+                if isinstance(augmented_kp, List):
+                    current_kp_texts.extend(augmented_kp)
+                else:
+                    current_kp_texts.append(augmented_kp)
 
-            arg_texts.append(arg_text)
-            kp_texts.append(kp_text)
-            labels.append(label)
+            for arg_text, kp_text in zip(current_arg_texts, current_kp_texts):
+                label: float
+                if (arg.id, kp.id) not in data.labels.keys():
+                    if self.label_policy == LabelPolicy.strict:
+                        label = 0
+                    elif self.label_policy == LabelPolicy.relaxed:
+                        label = 1
+                    else:
+                        raise Exception("Broken label policy.")
+                else:
+                    label = data.labels[arg.id, kp.id]
 
-    data = DataFrame()
-    data["text_a"] = arg_texts
-    data["text_b"] = kp_texts
+                arg_texts.append(arg_text)
+                kp_texts.append(kp_text)
+                labels.append(label)
 
-    if over_sample:
-        over_sampler = RandomOverSampler(random_state=42)
-        data, labels = over_sampler.fit_resample(data, labels)
+        data = DataFrame()
+        data["text_a"] = arg_texts
+        data["text_b"] = kp_texts
 
-    data["labels"] = labels
-    return data
+        if self.over_sample:
+            over_sampler = RandomOverSampler(random_state=self.seed)
+            data, labels = over_sampler.fit_resample(data, labels)
 
+        data["labels"] = labels
 
-def _arg_kp_pairs(data: Dataset) -> List[ArgumentKeyPointPair]:
-    pairs = [
-        (arg, kp)
-        for arg in data.arguments
-        for kp in data.key_points
-        if arg.topic == kp.topic and arg.stance == kp.stance
-    ]
-    return pairs
+        if self.shuffle:
+            data = data.sample(frac=1, random_state=self.seed)
+
+        return data
+
+    @staticmethod
+    def _prepare_dev_data(data: LabelledDataset) -> DataFrame:
+        pairs = [
+            (arg, kp)
+            for arg in data.arguments
+            for kp in data.key_points
+            if (arg.topic == kp.topic and arg.stance == kp.stance
+                and (arg.id, kp.id) in data.labels.keys())
+        ]
+        arg_texts: List[str] = []
+        kp_texts: List[str] = []
+        labels: List[float] = []
+        for arg, kp in pairs:
+            arg_texts.append(arg.text)
+            kp_texts.append(kp.text)
+            labels.append(data.labels[arg.id, kp.id])
+
+        data = DataFrame()
+        data["text_a"] = arg_texts
+        data["text_b"] = kp_texts
+        data["labels"] = labels
+        return data
+
+    @staticmethod
+    def _prepare_test_data(data: Dataset) -> \
+            Tuple[List[ArgumentKeyPointPair], List[List[str]]]:
+        pairs = [
+            (arg, kp)
+            for arg in data.arguments
+            for kp in data.key_points
+            if arg.topic == kp.topic and arg.stance == kp.stance
+        ]
+        texts = [[arg.text, kp.text] for arg, kp in pairs]
+        return pairs, texts
