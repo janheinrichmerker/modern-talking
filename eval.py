815e9485520b3676ea39a6ead8db3a5314180e32
Jan Heinrich Reimer <heinrich@reimer.family>
2021-07-16 16:59:06 +0200

    Add evaluation CLI for saved labels

---
+++ eval.py
@@ -0,0 +1,26 @@
+from modern_talking.data import download_kpa_2021_data
+from modern_talking.matchers.utils import setup_colab_tpu
+from modern_talking.pipeline import Pipeline
+from modern_talking.pipeline.cli import parse_pipeline_cli
+
+
+def evaluate(pipeline: Pipeline) -> None:
+    """
+    Evaluate matcher.
+    """
+    print(f"Train/evaluate matcher '{pipeline.matcher.slug}' "
+          f"with metric '{pipeline.metric.slug}'.")
+
+    setup_colab_tpu()
+
+    # Download datasets.
+    download_kpa_2021_data()
+
+    # Execute pipeline.
+    result = pipeline.evaluate()
+
+    print(f"Final score for metric {pipeline.metric.slug}: {result:.4f}")
+
+
+if __name__ == "__main__":
+    evaluate(parse_pipeline_cli())
